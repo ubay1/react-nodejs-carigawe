65278d861ded2829bf2508636932813bfb687675
ubay <ubay00804@gmail.com>
2021-01-21 10:11:05 +0700

    desain home

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,26 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/pulse.css';
+import Moment from 'moment'
+
+interface IPostJob {
+    nama_pt: string;
+    kebutuhan: string;
+    skill: string[],
+    lamaran_terakhir: string;
+}
 
 const Home = () => {
     const [menuHide, setmenuHide] = useState(true)
     const [scrolled,setScrolled] = React.useState(false);
     const [marginTopp, setmarginTopp] = useState('mt-24');
+    const [postJobs, setpostJobs] = useState([1,1,2,2,2,2,2,2,2,2,2,2])
+
+    const yearNow = Moment(new Date()).format('YYYY');
+
 
     const Header = () => {
         const handleScroll=() => {
@@ -27,157 +41,104 @@ const Home = () => {
         }, [])
         
         return(
-        <div className={`bg-white shadow-md flex items-center z-10 justify-between flex-wrap p-4  w-full pin-t`}
-        id="myHeader"
-        >
-            <div className="flex items-center flex-no-shrink text-black mr-6">
-                <Link className="text-4xl text-blue-600 tracking-wide ml-2 font-semibold font_damion" to="/">
-                    Cari Gawe
-                </Link>
-            </div>
-    
-            <div className="block lg:hidden">
-                <button 
-                    id="nav-toggle" className="flex items-center px-3 py-2 border rounded text-grey border-grey-dark hover:text-white hover:border-white"
-                    onClick={() => {
-                        setmenuHide(!menuHide)
-                    }}
-                >
-                    Menu
-                </button>
-            </div>
-    
             <div 
-                className={
-                    `w-full flex-grow lg:flex lg:items-center lg:w-auto pt-6 lg:pt-0
-                    ${menuHide === true ? 'hidden': 'flex'}
-                    `
-                }
+                className="bg-white shadow-md  z-20 p-4  w-full pin-t grid lg:grid-cols-lg-3rows-content"
+            id="myHeader"
             >
-                <ul className="list-reset lg:flex justify-start flex-1 items-center">
-                    <li className="mr-0">
-                        <a className="inline-block py-2 px-4 no-underline" href="#">Lowongan Terbaru</a>
-                    </li>
-                </ul>
-                <ul className="list-reset lg:flex justify-end flex-1 items-center">
-                    <li className="mr-0">
-                        <Link className="inline-block py-2 px-4 no-underline" to="/login">Masuk</Link>
-                    </li>
-                    <li className="mr-0">
-                        <Link className="inline-block py-2 px-4 no-underline" to="/register">Daftar</Link>
-                    </li>
-                </ul>
+                <div className="flex items-center flex-no-shrink text-black ">
+                    <Link className="text-4xl text-blue-600 tracking-wide font-semibold font_damion" to="/">
+                        Cari Gawe
+                    </Link>
+                </div>
+        
+                <div className="block lg:hidden">
+                    <button 
+                        id="nav-toggle" className="flex items-center px-3 py-2 border rounded text-grey border-grey-dark hover:text-white hover:border-white"
+                        onClick={() => {
+                            setmenuHide(!menuHide)
+                        }}
+                    >
+                        Menu
+                    </button>
+                </div>
+        
+                {/* <div 
+                    className={
+                        `w-full flex-grow lg:flex lg:items-center lg:w-auto pt-6 lg:pt-0
+                        ${menuHide === true ? 'hidden': 'flex'}
+                        `
+                    }
+                > */}
+                    <ul className="list-reset lg:flex justify-start flex-1 items-center">
+                        <li className="mr-0">
+                            <a className="pulse inline-block bg-blue-500 hover:bg-blue-600 text-sm rounded-lg text-white shadow-md py-2 px-4 no-underline" href="#">Lowongan Terbaru</a>
+                        </li>
+                    </ul>
+                    <ul className="list-reset lg:flex justify-end flex-1 items-center">
+                        <li className="mr-0">
+                            <Link className="inline-block py-2 px-4 no-underline" to="/login">Masuk</Link>
+                        </li>
+                        <li className="mr-0">
+                            <Link className="inline-block py-2 px-4 no-underline" to="/register">Daftar</Link>
+                        </li>
+                    </ul>
+                {/* </div> */}
             </div>
-        </div>
         )
     }
 
+    const Articles = () => (
+        <div className="px-8 py-8 bg-gray-50 -z-10 grid gap-4 lg:grid-cols-1 md:grid-cols-1 ">
+            {
+            postJobs.map((item: any, index: number) => {
+                return(
+                        <div 
+                            className="px-5 py-4  bg-white dark:bg-gray-800 shadow rounded-lg w-full"
+                        >
+                            <div className="flex mb-4">
+                            <img className="w-12 h-12 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" />
+                            <div className="ml-2 mt-0.5">
+                                <span className="block font-medium text-base leading-snug text-black dark:text-gray-100">Loyce Kuvalis</span>
+                                <span className="block text-sm text-gray-500 dark:text-gray-400 font-light leading-snug">16 December at 08:25</span>
+                            </div>
+                            </div>
+                            <p className="text-gray-800 dark:text-gray-100 leading-snug md:leading-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <div className="flex justify-between items-center mt-5">
+                            <div className="flex ">
+                            <span className="ml-1 text-gray-500 dark:text-gray-400  font-light">8</span>
+                            </div>  
+                            <div className="ml-1 text-gray-500 dark:text-gray-400 font-light">33 comments</div>
+                            </div>
+                        </div>
+                )
+            })
+            }
+        </div>
+    )
+
     return(
-        <div className="grid h-screen gap-4"
-            style={{
-                gridTemplateRows: '70px auto 30px'
-            }}
-        >
+        <div>
             <Header />
 
-            <div className="mx-4 grid gap-4 lg:grid-cols-3 md:grid-cols-2 ">
-                <div className="shadow-lg relative pb-2 h-auto w-full z-0 grid lg:grid-rows-lg-3rows-content"
-                >
-                    <div>
-                        <img className="h-full w-full object-cover rounded-r-none pb-5/6" src="https://ik.imagekit.io/q5edmtudmz/FB_IMG_15658659197157667_wOd8n5yFyXI.jpg" alt="bag" />
-                    </div>
-                    <div className="w-full h-full px-4 py-4 bg-white rounded-lg">
-                        <p className="text-xl font-bold text-gray-700">
-                            PT. Daur Ulang
-                        </p>
-                        <p className="text-sm my-2 font-semibold text-gray-700">
-                            membutuhkan Frontend Developer
-                        </p>                   
-                        <p className="text-sm text-gray-700">
-                            kualaifikasi:
-                        </p>
-                        <p>
-                            <li>bisa ReactJS/VueJS</li>
-                            <li>bisa menggunakan API</li>
-                            <li>paham nodejs sangat disukai</li>
-                        </p>
-                        <p className="text-sm mt-20">
-                            kirimkan lamaran sebelum 26-01-2021
-                        </p>
-                    </div>
+            {/* article dengan grid */}
+            <div className="grid lg:grid-cols-lg-3rows-content">
+                <div className="">
+                    <div className="bg-blue-100 h-auto my-8 mx-4 py-8 px-4">
+                        <div className=" text-sm font-semibold text-gray-700 tracking-wide mb-1">Posisi Pekerjaan</div>
+                        <input className="w-full mb-2 text-sm py-2 px-2 shadow rounded-lg border-gray-300 focus:outline-none focus:border-blue-500" type="" placeholder="frontend developer" />
 
-                    <div className="w-full pb-4 px-2 grid gap-2 lg:grid-cols-2">
-                        <button className="bg-blue-500 hover:bg-blue-400 py-3 text-white shadow-lg rounded-full lg:w-full">Lamar</button>
-                        <button className="bg-red-500 hover:bg-red-400 p-3 text-white shadow-lg rounded-full lg:w-full">Komentar</button>
+                        <div className=" text-sm font-semibold text-gray-700 tracking-wide mb-1">Kota</div>
+                        <input className="w-full mb-2 text-sm py-2 px-2 shadow rounded-lg border-gray-300 focus:outline-none focus:border-blue-500" type="" placeholder="Bekasi" />
+
+                        <button className="inline-block bg-blue-500 hover:bg-blue-600 text-sm rounded-lg text-white shadow mt-2 w-full py-2 px-4 no-underline">Cari</button>
                     </div>
                 </div>
-
-                <div className="shadow-lg relative pb-2 h-auto w-full z-0 grid lg:grid-rows-lg-3rows-content"
-                >
-                    <div>
-                        <img className="h-full w-full object-cover rounded-r-none pb-5/6" src="https://ik.imagekit.io/q5edmtudmz/FB_IMG_15658659197157667_wOd8n5yFyXI.jpg" alt="bag" />
-                    </div>
-                    <div className="w-full h-full px-4 py-4 bg-white rounded-lg">
-                        <p className="text-xl font-bold text-gray-700">
-                            PT. Daur Ulang
-                        </p>
-                        <p className="text-sm my-2 font-semibold text-gray-700">
-                            membutuhkan Frontend Developer
-                        </p>                   
-                        <p className="text-sm text-gray-700">
-                            kualaifikasi:
-                        </p>
-                        <p>
-                            <li>bisa ReactJS/VueJS</li>
-                            <li>bisa menggunakan API</li>
-                            <li>paham nodejs sangat disukai</li>
-                        </p>
-                        <p className="text-sm mt-20">
-                            kirimkan lamaran sebelum 26-01-2021
-                        </p>
-                    </div>
-
-                    <div className="w-full pb-4 px-2 grid gap-2 lg:grid-cols-2">
-                        <button className="bg-blue-500 hover:bg-blue-400 py-3 text-white shadow-lg rounded-full lg:w-full">Lamar</button>
-                        <button className="bg-red-500 hover:bg-red-400 p-3 text-white shadow-lg rounded-full lg:w-full">Komentar</button>
-                    </div>
-                </div>
-
-                <div className="shadow-lg relative pb-2 h-auto w-full z-0 grid lg:grid-rows-lg-3rows-content"
-                >
-                    <div>
-                        <img className="h-full w-full object-cover rounded-r-none pb-5/6" src="https://ik.imagekit.io/q5edmtudmz/FB_IMG_15658659197157667_wOd8n5yFyXI.jpg" alt="bag" />
-                    </div>
-                    <div className="w-full h-full px-4 py-4 bg-white rounded-lg">
-                        <p className="text-xl font-bold text-gray-700">
-                            PT. Daur Ulang
-                        </p>
-                        <p className="text-sm my-2 font-semibold text-gray-700">
-                            membutuhkan Frontend Developer
-                        </p>                   
-                        <p className="text-sm text-gray-700">
-                            kualaifikasi:
-                        </p>
-                        <p>
-                            <li>bisa ReactJS/VueJS</li>
-                            <li>bisa menggunakan API</li>
-                            <li>paham nodejs sangat disukai</li>
-                        </p>
-                        <p className="text-sm mt-20 w-">
-                            kirimkan lamaran sebelum 26-01-2021
-                        </p>
-                    </div>
-
-                    <div className="w-full pb-4 px-2 grid gap-2 lg:grid-cols-2">
-                        <button className="bg-blue-500 hover:bg-blue-400 py-3 text-white shadow-lg rounded-full lg:w-full">Lamar</button>
-                        <button className="bg-red-500 hover:bg-red-400 p-3 text-white shadow-lg rounded-full lg:w-full">Komentar</button>
-                    </div>
-                </div>
-
+                <Articles />
             </div>
+            
 
-            <div className="h-full flex items-center justify-center text-center text-xs text-gray-400">
-                2021 Cari Gawe
+            <div className="h-full py-8 bg-blue-400 flex items-center justify-center text-center text-sm text-white">
+                © {yearNow} Cari Gawe
             </div>
         </div>
     )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,9 +3,21 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      gridTemplateRows: {
+      gridTemplateColumns: {
         // Simple 16 column grid
-        'lg-3rows-content': '150px auto 50px',
+        'lg-3rows-content': '20% 60% 20%',
+      },
+      zIndex: {
+        '-10': '-10',
+        '-20': '-20',
+        '-30': '-30',
+        '-40': '-40',
+        '-50': '-50',
+        '-60': '-60',
+        '-70': '-70',
+        '-80': '-80',
+        '-90': '-90',
+        '-100': '-100',
       }
     },
   },
